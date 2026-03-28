17df33ec1f9d0a6fb05795fbcba0e0db00a0e6e4
eemendoza-2025061 <emendoza-2025061@kinal.edu.gt>
2026-03-27 18:35:17 -0600

    feat: agregando SecurityConfig

---
+++ src/main/java/com/eliasmendoza/Kinalapp/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.eliasmendoza.Kinalapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .httpBasic(withDefaults());
+        
+        http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
+
+        return http.build();
+    }
+}
