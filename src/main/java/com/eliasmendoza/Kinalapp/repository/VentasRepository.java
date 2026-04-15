c314eca92b26225ad984397592d959985cd16aef
emendoza-2025061 <emendoza-2025061@kinal.edu.gt>
2026-04-15 11:51:49 -0600

    feat: Se agrega VentasRepository

--- src/main/java/com/eliasmendoza/Kinalapp/repository/VentasRepository.java
+++ src/main/java/com/eliasmendoza/Kinalapp/repository/VentasRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // Repositorio para la entidad Ventas.
 public interface VentasRepository extends JpaRepository<Ventas, Long> {
-    // Long = codigoVenta
 }
