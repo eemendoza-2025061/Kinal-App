a1eedafc945156a736b7862fb3f59b8921825474
emendoza-2025061 <emendoza-2025061@kinal.edu.gt>
2026-04-15 11:49:46 -0600

    feat: Se agrega UsuarioRepository

--- src/main/java/com/eliasmendoza/Kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/eliasmendoza/Kinalapp/repository/UsuarioRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository  // Marca explícitamente como bean de repositorio (opcional, JpaRepository ya lo trae)
+@Repository 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     // Busca usuarios por username. Retorna List para evitar excepción si hay duplicados.
