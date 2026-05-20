2105430537377f9b928a7fe167188a9678a1c5eb
emendoza-2025061 <emendoza-2025061@kinal.edu.gt>
2026-05-19 18:19:36 -0600

    Delete: se elimina codigos repetitivos

--- src/main/java/com/eliasmendoza/Kinalapp/repository/DetalleVentaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eliasmendoza.Kinalapp.repository;
-
-import com.eliasmendoza.Kinalapp.entity.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Repositorio para DetalleVenta. Hereda métodos como save(), findAll(), deleteById().
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
